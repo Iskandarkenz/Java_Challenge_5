9076e818fd7e2aa80be5be3be518a8a171ac34d0
Iskandarkenz <miskandarrasyid11@gmail.com>
2021-10-22 14:38:40 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        Heroes hero = new Heroes();
+    
+        hero.setProfile("Iskandar", 17, 169);
+        hero.profile();
+        
+    
+        // jalankan aktivitas disini...
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+    
+        hero.lari();
+        hero.lari();
+
+        hero.makan();
+        hero.makan();
+        hero.makan();
+
+        hero.minum();
+
+        hero.lompat();
+        hero.lompat();
+        hero.lompat();
+
+        hero.duduk();
+        
+        // tampilkan status terkini...
+        hero.profile();
+      }
+    }
